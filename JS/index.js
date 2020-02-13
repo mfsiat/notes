caa069c9b9a1594cfef7d2f7d4355cb3edfade3c
Nasirul Islam <nasirul.siat@outlook.com>
2020-02-13 10:29:11 +0600

    linkedlist implemented and added some data

--- JS/index.js
+++ JS/index.js
@@ -17,12 +17,38 @@ class Node {
     }
 }
 
-const n1 = new Node(100);
-console.log(n1);
+// const n1 = new Node(100);
+// console.log(n1);
 
 class LinkedList {
     constructor() {
         this.head = null;
         this.size = 0;
     }
+
+    // Insert first node 
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+
+    // Insert last node 
+
+    // Insert at index 
+
+    // Get an index 
+
+    // Remove at index 
+
+    // Clear list 
+
+    // Print list data 
 }
+
+const ll = new LinkedList();
+
+ll.insertFirst(100);
+ll.insertFirst(200);
+
+console.log(ll);
+
